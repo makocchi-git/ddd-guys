3da75750fa4627008116c4484f0449c633c4e612
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2020-01-29 17:03:05 +0900

    add the reason why writing uuid unit test

--- go/domain/model/user/id_test.go
+++ go/domain/model/user/id_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/jupemara/ddd-guys/go/domain/model/user"
 )
 
+// NewIdのテスト、ほぼUUIDのテストなので不要って思うかもしれませんが、ライブラリの動作確認をしたり(go run xxx.goみたいに手元で確認すると再現性がない)、忘れた頃に使い方を見直す場所として時々書いたりします
+
 func TestNewId(t *testing.T) {
 	id, err := user.NewId()
 	if err != nil {
