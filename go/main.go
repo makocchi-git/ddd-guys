fb50ddb954da38b1966e222276b94894686f971e
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2020-02-27 23:33:24 +0900

    reply to main.go

--- go/main.go
+++ go/main.go
@@ -12,10 +12,16 @@ import (
 	// >> ちょっと深く掘りすぎかな・・
 	//    repository/user/register/id/provider がいいか、 repository/user/id/provier がいいか、悩むけども
 	//    id を provide する機能は register の一部で、それ以外からは使われ無さそうという意味でこっちのほうがいいのかな
+	// ここは悩ましいですね。まず、"深く掘りすぎ"ということはないです! どんどん掘っていきましょう!
+	// >id を provide する機能は register の一部で、それ以外からは使われ無さそうという意味
+	// コレDDDっぽい考え方ですね!
+	// ただ技術レイヤなので、もし深すぎるかなーと感じたら(実はpackage, ディレクトリ構造に関しては正解がないので、ドメインや、チーム構成、扱う技術領域に依存することもしばしばです)
+	// repository/user/id/uuid.go とかでもアリかなと思います!
 	idprovider "github.com/makocchi-git/ddd-guys/go/pkg/repository/user/register/id/provider"
 	// パッケージエイリアスは愚直にusecaseのがわかりやすいかもですね
 	// >> ここも同じく usecase/user/register にしてみる
 	//    domain と同じように複数の usecase が出てきた時に分ければいいので、ここでは usecase で定義
+	// +1 です!
 	usecase "github.com/makocchi-git/ddd-guys/go/pkg/usecase/user/register"
 	// >> まとめると
 	//    pkg/domain/user/...
@@ -23,6 +29,13 @@ import (
 	//    pkg/usecase/user/...
 	//    という風になり、わかりやすくなったかな？
 	//    むしろこういう風に合わせちゃうと、なんかしらの縛りっぽくなっちゃうのならば逆効果だけども・・
+	// 見やすくなりましたね!! user/ で切るのって domain/user がuserに関する内容だからという理由なので、usecase/user/register.go, usecase/user/delete.go
+	// みたいになってuserに関するapplication serviceというのがわかりやすくなったように思います!
+	// >なんかしらの縛り
+	// 複数のdomain/配下のモデルを扱うようなケースであったとしても、主体となってるのはどれかというのがあると思うので(例えば、user, credit_cardならuserがおそらく主体)、
+	// 主体側に合わせるのがよいかなと思います! ただ実際やっていって usecase/user 配下がどんどん増えてくるとかもありえるのでそうなったら、packageを細かく
+	// usecase/user_register (もとに戻ってるけどｗ) みたいに部分的に分けていってあげればいいかと!
+	// DDDではよく、開発しているうちにさらにしっくり来る名前とか、ディレクトリ名とかがひらめいたりすること多いので、ビジネス要件の文脈や言葉にマッチしてるかどうかが重要になります!!
 )
 
 func main() {
@@ -37,6 +50,7 @@ func main() {
 
 	// >> factory パターンで実装しなおし
 	//    だけどこれでいいのかどうか・・・
+	// いいと思います! スッキリして読みやすくなりました!!
 	idpRegister, ierr := idprovider.CreateIdProvider(*idp)
 	if ierr != nil {
 		log.Fatalf("Failed to create an id provider. raw error message: %v", ierr)
