a659abf4623077c7318aaa9e2bd11a73767d1f9f
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2020-03-03 20:42:44 +0900

    today's 2nd work

--- go/domain/model/user/user_repository.go
+++ go/domain/model/user/user_repository.go
@@ -4,4 +4,8 @@ package user
 type IUserRepository interface {
 	Store(*User) error
 	FindById(*Id) (*User, error)
+	// ChangeUserNameとかUpdateFirstNameみたいにひとつひとつのフィールドごとに
+	// application serviceやrepositoryのメソッドを増やすのはアンチパターン
+	// 受け取るのはusecaseですでに変更済みのUserオブジェクトだけを受け取る
+	Update(*User) error
 }
