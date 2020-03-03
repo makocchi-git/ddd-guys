7431d68ac34e032aaa3f68c2698c2efc6921bd19
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2020-03-03 20:34:47 +0900

    additional comment in today's session

--- go/usecase/user/find.go
+++ go/usecase/user/find.go
@@ -16,6 +16,8 @@ func NewUserFindUsecase(repository domain.IUserRepository) *UserFindUsecase {
 
 // 返す値はDTOでもいいし、そのまま返してもいい
 // ただそのままのドメインオブジェクトを返すと、このユースケースを使われる場所でもドメインのメソッドが叩かれてしまう可能性があるので注意
+// 下記はじめの段階でメソッドシグネチャ(ここでは `Execute(id string) (*Dto, error)` )のみを書いて、
+// コンパイルエラーを直していくような形で型定義をしていくスタイルだとビジネスルールに近い形のコードができあがるはずです
 func (u *UserFindUsecase) Execute(id string) (*Dto, error) {
 	userId := domain.NewId(id)
 	user, err := u.repository.FindById(userId)
@@ -33,4 +35,22 @@ type Dto struct {
 	Id        string
 	FirstName string
 	LastName  string
+}
+
+// こちらのパターンのように上位のレイヤー(UIやhttp handler)変更してほしくない値だけを
+// read onlyにしてガードしてあげるというパターンもアリえます
+type ReadOnlyIdDto struct {
+	id        string
+	FirstName string
+	LastName  string
+}
+
+func NewDto(id string) ReadOnlyIdDto {
+	return ReadOnlyIdDto{
+		id: id,
+	}
+}
+
+func (d ReadOnlyIdDto) Id() string {
+	return d.id
 }
