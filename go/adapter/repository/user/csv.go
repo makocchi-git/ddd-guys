c1f09479fc7d70f88d0df10339bfa790c66574b0
makocchi-git <makocchi@gmail.com>
2020-03-04 21:50:23 +0900

    implement Update() in csv repository

--- go/adapter/repository/user/csv.go
+++ go/adapter/repository/user/csv.go
@@ -3,8 +3,11 @@ package user
 import (
 	"encoding/csv"
 	"errors"
+	"fmt"
 	"io"
 	"os"
+
+	"github.com/gofrs/flock"
 
 	domain "github.com/jupemara/ddd-guys/go/domain/model/user"
 )
@@ -23,7 +26,7 @@ func (r *CsvRepository) Store(user *domain.User) error {
 	id := user.Id()
 	firstName := user.Name().FirstName()
 	lastName := user.Name().LastName()
-	file, err := os.OpenFile("/tmp/users.csv", os.O_WRONLY|os.O_APPEND, 0600)
+	file, err := os.OpenFile("/tmp/users.csv", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0600)
 	defer file.Close()
 	if err != nil {
 		return err
@@ -64,4 +67,61 @@ func (r *CsvRepository) FindById(id *domain.Id) (*domain.User, error) {
 		}
 	}
 	return nil, errors.New("Couldn't find specified user")
+}
+
+func (r *CsvRepository) Update(user *domain.User) error {
+
+	var newRecords [][]string
+	var err error
+
+	// 簡易的な lock の機構が必要かな
+	fileLock := flock.New("/tmp/users.csv")
+	locked, err := fileLock.TryLock()
+	if err != nil {
+		return err
+	}
+
+	// lock failed
+	if !locked {
+		return fmt.Errorf("csv file is locked. Please try again.")
+	}
+
+	defer fileLock.Unlock()
+
+	rfile, err := os.OpenFile("/tmp/users.csv", os.O_RDONLY, 0600)
+	if err != nil {
+		return err
+	}
+
+	reader := csv.NewReader(rfile)
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		if user.Id() == record[0] {
+			newRecords = append(newRecords, []string{
+				record[0],
+				user.Name().FirstName(),
+				user.Name().LastName(),
+			})
+		} else {
+			// no need to change the record
+			newRecords = append(newRecords, record)
+		}
+	}
+
+	// 書き込みの為に再度 Open
+	wfile, err := os.OpenFile("/tmp/users.csv", os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+	defer wfile.Close()
+	out := csv.NewWriter(wfile)
+
+	// WriteAll が error を返すので、そのまま return に渡す
+	return out.WriteAll(newRecords)
 }
--- go/main.go
+++ go/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
 
 	repository "github.com/jupemara/ddd-guys/go/adapter/repository/user"
 	usecase "github.com/jupemara/ddd-guys/go/usecase/user"
 )
 
-func register() {
-	firstName := "John"
-	lastName := "Smith"
+func register(firstName, lastName string) {
 	// ここでは直接アプリケーションサービス(ユースケース)を呼び出していますが、実際はCLIがあったりHTTPのハンドラーがあって、それらからfirstName, lastNameを取得する形になります
 	// 技術的複雑性は"すべて"DI(Dependency Injection: 依存性の注入)を使って外側から入れます
 	err := usecase.NewUserRegisterUsecase(
@@ -22,18 +22,46 @@ func register() {
 	if err != nil {
 		log.Fatalf("Failed to register user. raw error message: %v", err)
 	}
-	log.Println("Succeeded to register user")
+	log.Printf("Succeeded to register user [%s %s]\n", firstName, lastName)
 }
 
 func main() {
-	const id = "abe7da64-5d25-11ea-aa98-acde48001122"
-	user, err := usecase.NewUserFindUsecase(
+	const (
+		newFirstName = "Taro"
+		newLastName  = "Yamada"
+	)
+
+	// flag
+	var (
+		gen = flag.Bool("gen-user", false, "generate sample user data")
+		id  = flag.String("id", "", "target id for modify user name")
+	)
+	flag.Parse()
+
+	// generate user data and exit
+	if *gen {
+		register("John", "Doh")
+		register("Rob", "Pike")
+		register("Ken", "Thompson")
+		os.Exit(0)
+	}
+
+	if len(*id) < 1 {
+		log.Fatal("please set id for modify user name")
+	}
+
+	command := usecase.NewCommand(
+		*id,
+		newFirstName,
+		newLastName,
+	)
+
+	err := usecase.NewUserUpdateUsecase(
 		repository.NewCsvRepository(),
-	).Execute(id)
+	).Execute(command)
 	if err != nil {
 		log.Fatalf("unexepcted error occurred: %s", err)
 	}
-	log.Printf(`id: %s
-first name: %s
-last name: %s`, user.Id, user.FirstName, user.LastName)
+
+	log.Printf("Succeeded to change name to %s %s\n", newFirstName, newLastName)
 }
