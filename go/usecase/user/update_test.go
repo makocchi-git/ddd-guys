2aae149aa5af0d0d885c0a328547189fd3507f17
Kozzy Hasebe <hasebe@google.com>
2020-04-30 00:01:43 +0900

    add test cases for user update usecase

--- go/usecase/user/update_test.go
+++ go/usecase/user/update_test.go
@@ -1,6 +1,7 @@
 package user_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -19,21 +20,56 @@ func TestUserUpdateUsecase(t *testing.T) {
 
 var _ = Describe("UseUpdateUsecase", func() {
 	var (
-		ctrl       *gomock.Controller
-		repository domain.IUserRepository
-		usecase    *user.UserUpdateUsecase
+		ctrl         *gomock.Controller
+		repository   *mock.MockIUserRepository
+		usecase      *user.UserUpdateUsecase
+		existingUser *domain.User
 	)
 	BeforeEach(func() {
 		ctrl = gomock.NewController(GinkgoT())
 		repository = mock.NewMockIUserRepository(ctrl)
 		usecase = user.NewUserUpdateUsecase(repository)
+		existingUser, _ = domain.NewUser(domain.NewId("12345"), "Rob", "Pike")
 	})
 	AfterEach(func() {
 		ctrl.Finish()
 	})
 	Describe("#Execute", func() {
-		Context("with valid name", func() {})
-		Context("with empty first name", func() {})
-		Context("when repository occur unexpected error", func() {})
+		Context("with valid name", func() {
+			It("updates an User without error", func() {
+				command := user.NewCommand("12345", "Ken", "Thompson")
+				repository.EXPECT().FindById(gomock.Any()).Return(existingUser, nil).Times(1)
+				repository.EXPECT().Update(gomock.Any()).Return(nil).Times(1)
+				err := usecase.Execute(command)
+				Expect(err).To(BeNil())
+			})
+		})
+		Context("with empty first name and valid last name", func() {
+			It("updates an User without error", func() {
+				command := user.NewCommand("12345", "", "Thompson")
+				repository.EXPECT().FindById(gomock.Any()).Return(existingUser, nil).Times(1)
+				repository.EXPECT().Update(gomock.Any()).Return(nil).Times(1)
+				err := usecase.Execute(command)
+				Expect(err).To(BeNil())
+			})
+		})
+		Context("with empty first/last name", func() {
+			It("does not update User with no error", func() {
+				command := user.NewCommand("12345", "", "")
+				repository.EXPECT().FindById(gomock.Any()).Return(existingUser, nil).Times(1)
+				repository.EXPECT().Update(gomock.Any()).Return(nil).Times(0)
+				err := usecase.Execute(command)
+				Expect(err).To(BeNil())
+			})
+		})
+		Context("when repository occur unexpected error", func() {
+			It("fails with error when executing FindById()", func() {
+				command := user.NewCommand("12345", "Ken", "Thompson")
+				repository.EXPECT().FindById(gomock.Any()).Return(nil, errors.New("unexpected error")).Times(1)
+				repository.EXPECT().Update(gomock.Any()).Return(nil).Times(0)
+				err := usecase.Execute(command)
+				Expect(err).NotTo(BeNil())
+			})
+		})
 	})
 })
