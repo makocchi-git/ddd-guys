60e582c80c9e04bc1a245a876ea231c540c2a92a
Kozzy Hasebe <hasebe@google.com>
2020-05-05 10:59:02 +0900

    implement id for movie package in hackthon app

---
+++ hackthon/domain/movie/id_test.go
@@ -0,0 +1,50 @@
+package movie_test
+
+import (
+	"testing"
+
+	"github.com/jupemara/ddd-guys/hackthon/domain/movie"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestIdBdd(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "create id instance test")
+}
+
+var _ = Describe("New Id", func() {
+	Context("with empty id", func() {
+		It("fails to create Id and returns error", func() {
+			idValue := ""
+			id, err := movie.NewId(idValue)
+			Expect(id).To(BeNil())
+			Expect(err).NotTo(BeNil())
+		})
+	})
+	Context("with valid id", func() {
+		It("creates Id without error", func() {
+			idValue := "valid-id"
+			id, err := movie.NewId(idValue)
+			Expect(id).NotTo(BeNil())
+			Expect(err).To(BeNil())
+			Expect(id.Value()).To(Equal(idValue))
+		})
+	})
+	Context("with an id shorter than the criteria", func() {
+		It("fails to create Id and returns error", func() {
+			idValue := "short"
+			id, err := movie.NewId(idValue)
+			Expect(id).To(BeNil())
+			Expect(err).NotTo(BeNil())
+		})
+	})
+	Context("with an id longer than the criteria", func() {
+		It("fails to create Id and returns error", func() {
+			idValue := "longId-abcdefghijklmnopqrstuvwxyz"
+			id, err := movie.NewId(idValue)
+			Expect(id).To(BeNil())
+			Expect(err).NotTo(BeNil())
+		})
+	})
+})
---
+++ hackthon/go.mod
@@ -0,0 +1,9 @@
+module github.com/jupemara/ddd-guys/hackthon
+
+go 1.12
+
+require (
+	github.com/google/uuid v1.1.1
+	github.com/onsi/ginkgo v1.12.0
+	github.com/onsi/gomega v1.9.0
+)
