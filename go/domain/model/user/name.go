14917d48c523b879e68cc3f6e406b2b4a1cfc586
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2020-05-06 18:56:27 +0900

    Like JavaScript "every"

--- go/domain/model/user/name.go
+++ go/domain/model/user/name.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"github.com/jupemara/ddd-guys/go/domain/errors"
+	"github.com/jupemara/ddd-guys/go/util"
 )
 
 type Name struct {
@@ -10,6 +11,17 @@ type Name struct {
 }
 
 func NewName(firstName string, lastName string) (*Name, error) {
+	// every(元ネタはjavascriptです)を使ったバージョンの実装
+	if !util.Every(
+		len(firstName) > 0,
+		len(lastName) > 0,
+	) {
+		return nil, errors.New(
+			"user name error",
+			errors.UserNameError,
+		)
+	}
+
 	for _, v := range []bool{
 		len(firstName) < 1,
 		len(lastName) < 1,
